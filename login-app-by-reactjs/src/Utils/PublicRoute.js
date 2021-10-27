52fd101f047c5f8b81a4ead9acfd22a06d61f598
Rajat Khandelwal <rajatkhandelwal8013@gmail.com>
2021-10-27 16:50:43 -0700

    Initial commit

---
+++ login-app-by-reactjs/src/Utils/PublicRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { getToken } from "./Common";
+
+function PublicRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !getToken() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/dashboard" }} />
+        )
+      }
+    />
+  );
+}
+
+export default PublicRoute;
